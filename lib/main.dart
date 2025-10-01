096beef389c764f8fda7bd288eaadea10edc2f20
NimasSeptiandini <septyandini921@gmail.com>
2025-10-01 10:09:26 +0700

    Praktikum 4: Implementasi image section

--- lib/main.dart
+++ lib/main.dart
@@ -73,8 +73,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'images/salarDeUyuni.jpeg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
             textSection,
